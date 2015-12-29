c9e5980eea180915e0ad249ff58e7ccedc0536e5
dickinsonmr1 <dickinsonmr1@gmail.com>
2015-12-28 22:40:23 -0500

    some ASP.NET config stuff

---
+++ src/dance-combo-generator/Models/Move.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dance_combo_generator.Models
+{
+    public enum MoveClassification
+    {
+        Basic,
+        Lollies,
+        OutAndIn,
+        TossOut,
+        AdLib,
+        Progressive    
+    }
+
+    public class Move
+    {
+        public string Title { get; set; }
+    }
+}
--- src/dance-combo-generator/Startup.cs
+++ src/dance-combo-generator/Startup.cs
@@ -15,6 +15,7 @@ namespace dance_combo_generator
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,10 +23,14 @@ namespace dance_combo_generator
         {
             app.UseIISPlatformHandler();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseFileServer();
+            app.UseMvc();
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
 
         // Entry point for the application.
